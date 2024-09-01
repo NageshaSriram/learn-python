459812cc41cefdf83f8745f283cd796ac31b28d2
Nagesha S <43410978+NageshaSriram@users.noreply.github.com>
2024-09-01 16:45:31 +0530

    Basics of Python

---
+++ app.py
@@ -0,0 +1,91 @@
+# age = 20
+# price = 19.95
+# first_name = "Nagesha Tejaswini"
+# is_online = True
+# print(age)
+from sympy.polys.groebnertools import lbp_sub
+
+# name = input("What is your name? ")
+# print("Hello " + name)
+
+# birth_year = input("Enter your birth year: ")
+# age = 2024 - int(birth_year)
+# print(age)
+
+# first_number = float(input("First: "))
+# second = float(input("Second: "))
+# sum1 = first_number + second
+# print("Sum "+ str(sum1))
+
+# course = "Python for beginners"
+# print('Python' in course)
+# print(course)
+
+# print(12 ** 2)
+
+# x = 100
+# x = x + 3
+# x //= 3
+# print(x)
+
+# x = (10 + 3) * 2
+# print(x)
+
+# x = 2 != 2
+# print(x)
+
+# price = 25
+# print(price and 100)
+
+# temperature = 6
+# if temperature > 30:
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+# elif temperature > 20:
+#     print("It's a nice day")
+# elif temperature > 10:
+#     print("It's a bit cold")
+# else:
+#     print("It's cold")
+# print("Done")
+
+# 1kg = 2.20462 lbp
+# weight = float(input("Please enter your weight: "))
+# weight_unit = input("(K)g or (L)bs: ")
+# if weight_unit.lower() == 'l':
+#     user_weight = weight / 2.20462
+#     print("Weight in Kg: " + str(user_weight))
+# elif weight_unit.lower() == 'k':
+#     user_weight = weight * 2.20462
+#     print("Weight in Lbs: " + str(user_weight))
+# else:
+#     print("Weight unit is wrong")
+
+# i = 1
+# while i <= 10:
+#     print(i * '*')
+#     i += 1
+
+# names = ["John", "Bob", "Mosh", "Nag", "Tej"]
+# names[0] = "Jon"
+# print(names[0:3])
+# print(names)
+
+# numbers = [1,2,3,4,5]
+# print(len(numbers))
+
+# numbers = [1,2,3,4,5]
+# for item in numbers:
+#     print(item)
+#
+# i = 0
+# while i < len(numbers):
+#     print(numbers[i])
+#     i += 1
+
+# numbers = range(5, 10, 2)
+# for item in range(5):
+#     print(item)
+
+numbers = (1,2,3,2)
+print(numbers.count(2))
